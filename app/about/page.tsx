9bf2237f74b1f1f530d3b26f6db77583b0a78c26
youssidrissi <youssrakalamidrissi@gmail.com>
2026-03-18 12:49:36 +0000

    Add about page

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Link from "next/link";
+import { ArrowRight, Users, Briefcase, Award, Zap } from "lucide-react";
+import { useTranslation } from "@/hooks/useTranslation";
+
+export default function AboutPage() {
+  const { t } = useTranslation();
+
+  return (
+    <div className="min-h-screen">Test</div>
+  );
+}
